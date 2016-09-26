37a1440737e908a01a7369f1260480a4ece01537
Ubuntu <ubuntu@ip-172-31-33-9.ec2.internal>
2016-09-26 17:47:45 +0000

    Integrating front end

--- TagDemo/services.py
+++ TagDemo/services.py
@@ -1,7 +1,11 @@
 import os
 import sqlite3
+import sys
+import traceback
+import json
+from array import *
 from flask import Flask, request, session, g, redirect, url_for, abort, \
-        render_template, flash
+        render_template, flash, Response
 
 from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.orm.exc import MultipleResultsFound
@@ -14,9 +18,26 @@ from TagDemo import tagdemo, db, models
 def default():
 	return "{OK}"
 
+@tagdemo.route('/getallpoints', methods = ['GET', 'POST'])
+def getallpoints():
+	points = models.Point.query.all()
+
+	fullPayload = []  
+
+	for point in points:
+		resultObjJSON = json.loads(point.results)
+		fullPayload.append(resultObjJSON)
+		print("Point:" + point.results)
+
+	result = {"fullPayload" : fullPayload}
+
+	print("Points:" + json.dumps(result))
+	return Response(json.dumps(result), mimetype='application/json') 
+
 @tagdemo.route('/savepoint', methods = ['GET', 'POST'])
 def savepoint():
         pointRequest = request.get_json(force=True)
+	requestText = request.data
 
 	tagId = pointRequest["tagId"]
 
@@ -24,6 +45,7 @@ def savepoint():
 		return "{\"Error\":\"No TagId Specified\"}"
 
 	try:
+		print "Saving tag info..."
 		if pointRequest["lastUpdate"] is not None:
 			g.lastUpdate = datetime.strptime(pointRequest["lastUpdate"], '%Y-%m-%dT%H:%M:%S.%fZ')
 		else:
@@ -31,15 +53,42 @@ def savepoint():
 
 		g.tag = models.Tag.query.filter(models.Tag.tagId==tagId).one() # g.-global namespace
 		g.tag.lastUpdate = g.lastUpdate
+		print "Updating tag..."
 		db.session.commit()
-		return "{\"Success\":\"Updated tagId: "+g.tag.tagId+"\"}"
 	except MultipleResultsFound, e:
 		return "{\"Error\":\"DB Corruption - multiple TagId Records\"}"
 	except NoResultFound, e:
+		print "New tag..."
 		g.tag = models.Tag(tagId=pointRequest["tagId"], lastUpdate=g.lastUpdate)
 		db.session.add(g.tag)
 		db.session.commit()
-		return "{\"Success\":\"Added tagId: "+g.tag.tagId+"\"}"	
-				
+	except:
+		return "{\"Error\":\"General error with point metadata\"}"
+		
+		
+	try:
+		print "Saving point..."
+		if pointRequest["results"] is not None:
+			result = pointRequest["results"][0]
+			lat = result["geometry"]["location"]["lat"]
+			lng = result["geometry"]["location"]["lng"]
+
+			point = models.Point(tagId=g.tag.tagId, tagIdNum=g.tag.id, parentTag = g.tag)
+			point.sendTime = g.tag.lastUpdate
+			point.recvTime = datetime.now()
+			point.status = pointRequest["status"]
+			point.lat = lat
+			point.lon = lng
+			point.results = requestText  
+			db.session.add(point)
+			db.session.commit()
+		else:
+			return( "{\"Error:\":\"No point result\"}" )
+
+	except NoResultFound, e:
+		e = sys.exc_info()[0]
+   		traceback.print_exc()
+		return( "{\"Error:\":\"%s\"}" % e )
+	
 
  	return "{\"status\":\"OK\"}"
